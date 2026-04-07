1d24adff0e3044417ddd770cd2b92e4f8108d8bd
Andreas <andreasssolli@gmail.com>
2026-04-07 13:46:14 +0200

    Fixed hearty command

--- cogs/forge.py
+++ cogs/forge.py
@@ -443,7 +443,8 @@ class Forge(commands.Cog):
     )
     async def hearty(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            dedent("""\<:helmaqw:1487000474014974054> **Hearty**
+            dedent("""\
+<:helmaqw:1487000474014974054> **Hearty**
         > Gives more HP.
         - 📝 Must have completed the ['The Gaol of Eternal Torment and Misery'](http://aqwwiki.wikidot.com/undead-cashfear-s-quests#GaolCell)
         - 📝 Rank 7 Grimskull Trolling to purchase these enhancements.
