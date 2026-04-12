975b9133f1c1ce05ae09a10977e53c3b60607669
Andreas <andreasssolli@gmail.com>
2026-04-12 21:35:36 +0200

    Added checking for users to ensure they are not new accs

--- user_verification/verification_buttons.py
+++ user_verification/verification_buttons.py
@@ -1,5 +1,6 @@
 import discord
 
+from config import OFFICER_CHANNEL_ID
 from user_verification.verification_modal import VerificationModal
 
 
@@ -9,10 +10,42 @@ class VerificationButtonView(discord.ui.View):
 
     @discord.ui.button(label="Verify me", style=discord.ButtonStyle.success, emoji="✅")
     async def verify(self, interaction: discord.Interaction, _):
+        account_age = (discord.utils.utcnow() - interaction.user.created_at).days
+        if account_age < 10:
+            guild = interaction.guild
+            if not guild:
+                return
+            oathsword_channel = guild.get_channel(OFFICER_CHANNEL_ID)
+            if oathsword_channel and isinstance(oathsword_channel, discord.TextChannel):
+                await oathsword_channel.send(
+                    f"{interaction.user.mention} is trying to verify but their account is too new."
+                )
+
+            await interaction.response.send_message(
+                "Your account was recently created. You cannot verify at this momeny.",
+                ephemeral=True,
+            )
+            return
         await interaction.response.send_modal(VerificationModal(action="verify"))
 
     @discord.ui.button(
         label="I want to join Oath", style=discord.ButtonStyle.primary, emoji="🛡️"
     )
     async def join_guild(self, interaction: discord.Interaction, _):
+        account_age = (discord.utils.utcnow() - interaction.user.created_at).days
+        if account_age < 10:
+            guild = interaction.guild
+            if not guild:
+                return
+            oathsword_channel = guild.get_channel(OFFICER_CHANNEL_ID)
+            if oathsword_channel and isinstance(oathsword_channel, discord.TextChannel):
+                await oathsword_channel.send(
+                    f"{interaction.user.mention} is trying to verify but their account is too new."
+                )
+
+            await interaction.response.send_message(
+                "Your account was recently created. You cannot verify at this momeny.",
+                ephemeral=True,
+            )
+            return
         await interaction.response.send_modal(VerificationModal(action="join"))
