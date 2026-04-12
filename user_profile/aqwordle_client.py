9b19a23fe5b2b5b859a276854ace840a906683d8
Andreas <andreasssolli@gmail.com>
2026-04-12 14:32:32 +0200

    Fix to aqwordle db

--- user_profile/aqwordle_client.py
+++ user_profile/aqwordle_client.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from google.cloud.firestore import Client
 
 cred = credentials.Certificate("firebase_key2.json")
-firebase_admin.initialize_app(cred)
 
-db: Client = firestore.client()
+if "aqwordle" not in firebase_admin._apps:
+    app = firebase_admin.initialize_app(cred, name="aqwordle")
+else:
+    app = firebase_admin.get_app("aqwordle")
+
+db = firestore.client(app=app)
