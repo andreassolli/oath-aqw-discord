990c565de03cd923875f9fd5bfc5c46b135a8854
Andreas <andreasssolli@gmail.com>
2026-05-10 11:53:33 +0200

    Trying fix to blackjack and donate

--- economy/gamba/blackjack_view.py
+++ economy/gamba/blackjack_view.py
@@ -352,6 +352,6 @@ class BlackjackView(discord.ui.View):
         await self.payout(interaction.user.id, -(self.wager // 2))
 
         self.stop()
-        await interaction.message.edit(
+        await self.message.edit(
             content=f"You surrendered and got back {self.wager // 2}.", view=None
         )
