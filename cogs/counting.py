183fed83626eb169051a452974cdefde66ef5d70
Andreas <andreasssolli@gmail.com>
2026-05-20 17:02:09 +0200

    Fix to guild stuff

--- cogs/counting.py
+++ cogs/counting.py
@@ -11,8 +11,6 @@ class CountingCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.author.bot:
-            return
 
         if message.channel.id == COUNTING_CHANNEL_ID:
             await handle_counting_message(message)
