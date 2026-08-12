4a5718e0ec68f85dfbb793ad8db07b05126e2853
Andreas <andreasssolli@gmail.com>
2026-08-12 23:59:37 +0800

    Embed needs to be removed

--- ticket_help/dashboard/updater.py
+++ ticket_help/dashboard/updater.py
@@ -16,7 +16,7 @@ async def update_dashboard(client: discord.Client):
 
     async for msg in channel.history(limit=10):
         if msg.author == client.user and msg.embeds:
-            await msg.edit(view=view)
+            await msg.edit(embeds=[], view=view)
             return
 
     await channel.send(view=view)
